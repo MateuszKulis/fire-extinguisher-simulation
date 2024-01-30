3ceede27147313eaa77dfc95c9b8d74095f4ef7b
pantrojan <mati22100@op.pl>
2024-01-30 14:04:25 +0100

    create a firebehaviour

---
+++ Assets/Scripts/FireSystem/FireBehavior.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+public class FireBehavior : MonoBehaviour
+{
+    public float fireLife = 1.0f;
+    private bool isBeingExtinguished = false;
+    private ParticleSystem fireParticleSystem;
+
+    void Start()
+    {
+        fireParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (isBeingExtinguished)
+        {
+            fireLife -= Time.deltaTime / 6.0f;
+            fireLife = Mathf.Max(fireLife, 0.0f);
+        }
+        else
+        {
+            fireLife += Time.deltaTime / 10.0f;
+            fireLife = Mathf.Min(fireLife, 1.0f);
+        }
+
+        UpdateFireVisual();
+    }
+
+    public void StartExtinguishing()
+    {
+        isBeingExtinguished = true;
+    }
+
+    public void StopExtinguishing()
+    {
+        isBeingExtinguished = false;
+    }
+
+    private void UpdateFireVisual()
+    {
+        var main = fireParticleSystem.main;
+        main.startSize = Mathf.Lerp(0.5f, 1.0f, fireLife);
+        main.startColor = Color.Lerp(Color.gray, Color.red, fireLife);
+    }
+}
